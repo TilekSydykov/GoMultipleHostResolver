e2d584f10952b30bbce4d5b8d9440160e047b499
TilekSydykov <tilek.sydykov@gmail.com>
2020-02-21 17:33:47 +0600

    df

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/valyala/fasthttp"
 	"time"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -11,6 +13,8 @@ func main() {
 		switch string(ctx.Path()) {
 		case "/":
 			index(ctx)
+		case "/{id}":
+			g(ctx)
 		default:
 			ctx.Error("not found", fasthttp.StatusNotFound)
 		}
@@ -21,8 +25,16 @@ func main() {
 
 func index(ctx *fasthttp.RequestCtx)  {
 	start := time.Now()
+	db, _ := sql.Open("mysql", "icamp:itcamp@/db_nodejs_1")
+	db.Prepare("select * from users")
 	fmt.Fprintf(ctx, "Hi there! RequestURI is %q %q", ctx.RequestURI(), ctx)
 	elapsed := time.Since(start)
 
 	fmt.Printf("execution time %q  || host: %q \n", elapsed, ctx.Host())
 }
+
+func g(ctx *fasthttp.RequestCtx){
+	fmt.Fprintf(ctx, "another uri")
+}
+
+
